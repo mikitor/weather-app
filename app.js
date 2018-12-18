027efee9f5f5a762e8f8994dda671724093cdcb0
Nick Torbich <nicktorbich@protonmail.com>
2018-12-18 16:12:05 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const request = require('request');
+
+request({
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=KEY&location=1301%20lombard%20street%20philadelphia`,
+  json: true,
+}, (error, response, body) => {
+  const {
+    latLng, street, adminArea4, adminArea3, adminArea1,
+  } = body.results[0].locations[0];
+  console.log(`Address: ${street}, ${adminArea4}, ${adminArea3}, ${adminArea1}`);
+  console.log(`Latitude: ${latLng.lat}`);
+  console.log(`Longitude: ${latLng.lng}`);
+});
