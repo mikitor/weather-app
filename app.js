798ce5b83179a492704fa581b399d851c27d9d69
Nick Torbich <nicktorbich@protonmail.com>
2018-12-18 16:16:07 +0100

    Add environmental variable for storing API_KEY

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const request = require('request');
 
 request({
-  url: `http://www.mapquestapi.com/geocoding/v1/address?key=KEY&location=1301%20lombard%20street%20philadelphia`,
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=${process.env.KEY}&location=1301%20lombard%20street%20philadelphia`,
   json: true,
 }, (error, response, body) => {
   const {
