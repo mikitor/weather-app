25180f968aa73c91b6b279792895e340efb4a121
Nick Torbich <nicktorbich@protonmail.com>
2018-12-18 20:48:40 +0100

    Encoding user input

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 require('dotenv').config();
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    address: {
+      description: 'Get the weather at this address',
+      demand: true,
+      alias: 'a',
+      string: true,
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+const encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: `http://www.mapquestapi.com/geocoding/v1/address?key=${process.env.KEY}&location=1301%20lombard%20street%20philadelphia`,
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=${process.env.KEY}&location=${encodedAddress}`,
   json: true,
 }, (error, response, body) => {
   const {
